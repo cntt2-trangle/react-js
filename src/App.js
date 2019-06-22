197859e7b3d92610d548eaf91d898e4b980114f2
Admin <le7207902@gmail.com>
2019-06-22 16:22:51 +0700

    [PHAN-04] 011 Cach viet JSX

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+const a1 = "Kit";
+class App1 extends Component {
+  render() {
+    return (
+      <div>
+        <h3 className="nut"> My name is {a1}</h3>
+        <div className="form-check">
+          <label className="form-check-label">
+            <input type="checkbox" className="form-check-input" name id defaultValue="checkedValue" defaultChecked/>
+            Display value
+          </label>
+        </div>
+      </div>
+    );
+  }
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <App1></App1>
         <a
           className="App-link"
           href="https://reactjs.org"
