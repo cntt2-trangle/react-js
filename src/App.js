5d9c47bcc7fc0c1fbef31508d17d9aaf9f1040e2
Admin <le7207902@gmail.com>
2019-06-22 18:15:28 +0700

    [PHAN-05] 016 Hoan thien phan giao dien

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import TopMenu from './Component/topmenu/TopMenu.js';
+import Header from './Component/header/Header';
+import Content from './Component/content/Content';
+import Footer from './Component/footer/Footer';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <TopMenu></TopMenu>
+        <Header></Header>
+        <Content tieude="Cach su dung props bang class" vitri1="order-lg-2" image ="img/01.jpg"></Content>
+        <Content tieude="Tin so 2" image ="img/02.jpg"></Content>
+        <Content tieude="Tin so 3" image ="img/03.jpg"></Content>
+        <Footer></Footer>
+      </div>
+    );
+  }
 }
+
 
 export default App;
