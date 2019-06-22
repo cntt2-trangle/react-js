e7515fb3e41219207bb417cc909609b8d3acaef3
Admin <le7207902@gmail.com>
2019-06-22 16:02:08 +0700

    [PHAN-03] 010 Phuong phap so 2 thao tac voi props

--- src/App.js
+++ src/App.js
@@ -17,6 +17,24 @@ function NumberOne(props){
   );
 }
 
+// Cach 2, su dung class de dinh nghia va thao tac voi props
+class NumberTwo extends Component {
+  render() {
+    return (
+      <div className="col-6">
+        <div className="card">
+          <img className="card-img-top" src={this.props.linkimg} alt=""/>
+          <div className="card-body">
+            <h4 className="card-title">{this.props.title}</h4>
+            <p className="card-text">{this.props.text}</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+
 function App() {
   return (
     <div className="App">
@@ -34,6 +52,9 @@ function App() {
       </header>
       <NumberOne title="Coffee" linkimg="https://product.hstatic.net/1000075078/product/vnese_coffee_large.jpg"></NumberOne>
       <NumberOne title="Smoothie" linkimg="https://product.hstatic.net/1000075078/product/mango_smoothie_large.jpg"></NumberOne>
+      <NumberTwo title="Matcha Tea" linkimg="https://product.hstatic.net/1000075078/product/matcha_macchiato_large.jpg" text="Bột trà xanh Matcha thơm lừng hảo hạng cùng lớp Macchiato béo ngậy là một sự kết hợp tuyệt vời."></NumberTwo>
+      <NumberTwo title="Americano" linkimg="https://product.hstatic.net/1000075078/product/americano_large.jpg" text="Americano được pha chế bằng cách thêm nước vào một hoặc hai shot Espresso để pha loãng độ đặc của cà phê, từ đó mang lại hương vị nhẹ nhàng, không gắt mạnh và vẫn thơm nồng nàn."></NumberTwo>
+      <NumberTwo title="Mocha đá xay" linkimg="https://product.hstatic.net/1000075078/product/mocha_ice_blended_large.jpg" text="Sự hoà quyện mịn màng giữa vị đắng thanh của cà phê và vị đắng ngọt của chocolate, cùng vị béo của kem tươi và sữa tươi hòa quyện."></NumberTwo>
     </div>
   );
 }
