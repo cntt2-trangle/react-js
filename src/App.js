598bacafb43515fd5c645d445d0871d75a422a28
Admin <le7207902@gmail.com>
2019-06-22 15:46:36 +0700

    [PHAN-03] 009 Khai niem props trong component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+
+// Su dung props cach 1
+function NumberOne(props){
+  return (
+    <div className="col-6">
+      <div className="card">
+        <img className="card-img-top" src={props.linkimg} alt=""/>
+        <div className="card-body">
+          <h4 className="card-title">{props.title}</h4>
+          <p className="card-text">Text</p>
+        </div>
+      </div>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Welcome to ReactJS</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -19,6 +32,8 @@ function App() {
           Learn React
         </a>
       </header>
+      <NumberOne title="Coffee" linkimg="https://product.hstatic.net/1000075078/product/vnese_coffee_large.jpg"></NumberOne>
+      <NumberOne title="Smoothie" linkimg="https://product.hstatic.net/1000075078/product/mango_smoothie_large.jpg"></NumberOne>
     </div>
   );
 }
