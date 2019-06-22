f6d5512b2eb3187177d8759b7ded3f6a8593ebd1
Admin <le7207902@gmail.com>
2019-06-22 14:28:53 +0700

    [PHAN-02] 006 Dinh nghia component cach so 1

--- src/App.js
+++ src/App.js
@@ -2,23 +2,31 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function One(){
+  return (
+    <div>
+      <h1>Cach so 1</h1>
+      <h3>Kieu ham binh thuong</h3>
+    </div>
+  );
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+        <h1>Welcome to ReactJS</h1>
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
+      <One></One>
     </div>
   );
 }
