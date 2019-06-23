611b7f456b6ba696063a7a7a30d2048d33591939
Admin <le7207902@gmail.com>
2019-06-23 12:16:08 +0700

    [PHAN-08] 022 Su dung thuoc tinh Default Value

--- src/Component/content/Content.js
+++ src/Component/content/Content.js
@@ -32,9 +32,12 @@ class Content extends Component {
     renderForm = () => (
         <div className="row">
             <div className="form-group">
-            <input type="text" name="ten" className="form-control" />
-            <div className="btn btn-success" onClick={() => this.saveClick()}>Save</div>
+                <input defaultValue={this.props.tieude} type="text" name="ten" className="form-control" />
             </div>
+            <div className="form-group">
+                <div className="btn btn-success" onClick={() => this.saveClick()}>Save</div>
+            </div>
+           
         </div>
     )
         
