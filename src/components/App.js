6c146bb1c229fef2741731cb8e308d8b98531083
Admin <le7207902@gmail.com>
2019-06-25 17:48:47 +0700

    [PHAN-010] 033 Tao Component Home, Footer, Nav

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import './../css/App.css';
+import Nav from './Nav';
+
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Nav></Nav>
+      </div>
+    );
+  }
+}
+
+export default App;
