f826c706ef60f6d6477edfd81bca90f20fc5f1ca
Admin <le7207902@gmail.com>
2019-06-28 16:29:35 +0700

    [PHAN-024] 072 Lay du lieu thanh vien moi bang su kien onChange

--- src/Components/AddUser.js
+++ src/Components/AddUser.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
 
 class AddUser extends Component {
+    isChange = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;
+        console.log(name);
+        console.log(value);
+    }
+    
     kiemTraTrangThai = () => {
         if(this.props.hienThiForm === true) {
             return (
@@ -9,16 +16,16 @@ class AddUser extends Component {
                         <div className="card-header">Thêm mới User vào hệ thống</div>
                         <div className="card-body text-primary">
                             <div className="form-group">
-                            <input type="text" className="form-control" placeholder="Tên User" />
+                            <input type="text" onChange={(event) => this.isChange(event)} name="name" className="form-control" placeholder="Tên User" />
                             </div>
                             <div className="form-group">
-                            <input type="text" className="form-control" placeholder="Điện thoại" />
+                            <input type="text" onChange={(event) => this.isChange(event)} name="tel" className="form-control" placeholder="Điện thoại" />
                             </div>
                             <div className="form-group">
-                            <select className="custom-select" required>
+                            <select className="custom-select" onChange={(event) => this.isChange(event)} name="permission" required>
                                 <option value>Chọn quyền mặc định</option>
                                 <option value={1}>Admin</option>
-                                <option value={2}>Moderator</option>
+                                <option value={2}>Modrator</option>
                                 <option value={3}>Normal</option>
                             </select>
                             </div>
