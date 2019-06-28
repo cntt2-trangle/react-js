22cfd5d3fa1784489c22cbc935e29cd74b4e5e83
Admin <le7207902@gmail.com>
2019-06-28 15:26:31 +0700

    [PHAN-024] 066 Tu dong co gian giao dien su dung col

--- src/Components/AddUser.js
+++ src/Components/AddUser.js
@@ -4,24 +4,26 @@ class AddUser extends Component {
     kiemTraTrangThai = () => {
         if(this.props.hienThiForm === true) {
             return (
-                <div className="card boder-primary mb-3 mt-2">
-                    <div className="card-header">Thêm mới User vào hệ thống</div>
-                    <div className="card-body text-primary">
-                        <div className="form-group">
-                        <input type="text" className="form-control" placeholder="Tên User" />
+                <div className="col">
+                    <div className="card boder-primary mb-3 mt-2">
+                        <div className="card-header">Thêm mới User vào hệ thống</div>
+                        <div className="card-body text-primary">
+                            <div className="form-group">
+                            <input type="text" className="form-control" placeholder="Tên User" />
+                            </div>
+                            <div className="form-group">
+                            <input type="text" className="form-control" placeholder="Điện thoại" />
+                            </div>
+                            <div className="form-group">
+                            <select className="custom-select" required>
+                                <option value>Chọn quyền mặc định</option>
+                                <option value={1}>Admin</option>
+                                <option value={2}>Moderator</option>
+                                <option value={3}>Normal</option>
+                            </select>
+                            </div>
+                            <div className="btn btn-block btn-primary">Thêm mới</div>
                         </div>
-                        <div className="form-group">
-                        <input type="text" className="form-control" placeholder="Điện thoại" />
-                        </div>
-                        <div className="form-group">
-                        <select className="custom-select" required>
-                            <option value>Chọn quyền mặc định</option>
-                            <option value={1}>Admin</option>
-                            <option value={2}>Moderator</option>
-                            <option value={3}>Normal</option>
-                        </select>
-                        </div>
-                        <div className="btn btn-block btn-primary">Thêm mới</div>
                     </div>
                 </div>
             )
@@ -29,11 +31,9 @@ class AddUser extends Component {
     }
     render() {
         return (
-            <div className="col-3">
-                <div>
-                    {/* {this.hienThiNut()} */}
-                    {this.kiemTraTrangThai()}
-                </div>
+            <div>
+                {/* {this.hienThiNut()} */}
+                {this.kiemTraTrangThai()}
             </div>
         );
     }
--- src/Components/TableData.js
+++ src/Components/TableData.js
@@ -9,7 +9,7 @@ class TableData extends Component {
     render() {
         console.log(this.props.dataUserProps);
         return (
-            <div className="col-9">
+            <div className="col">
                 <table className="table table-striped table-hover">
                     <thead>
                     <tr>
